fff0fcfe95b93914ee75f3c17157d8c471cd576d
sutaiyun <sutaiyun@longcheer.net>
2016-08-01 21:39:50 +0800

    增加第一个 测试用例，  测试失败， 等待增加业务代码！

---
+++ src/main/java/actestDemo/actest/TrafficLightsAcTest.java
@@ -0,0 +1,15 @@
+/**
+ * Created by sutaiyun on 2016/8/1.
+ */
+
+package actestDemo.actest;
+
+public class TrafficLightsAcTest {
+    public void setPreviousStates(String states) {
+        System.out.println(states);
+    }
+
+    public String nextStates() {
+        return null;
+    }
+}
