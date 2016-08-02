6f942b9f110f741ff172b54421444fbf4399b882
sutaiyun <sutaiyun@longcheer.net>
2016-08-02 14:13:15 +0800

    将lightStateEditor的测试 参数化。   测试失败，  （因为   Editor 的业务代码  还没有更新， 还是写死的）

--- src/test/java/actestDemo/domain/LightStateEditorTest.java
+++ src/test/java/actestDemo/domain/LightStateEditorTest.java
@@ -1,20 +1,52 @@
 package actestDemo.domain;
 
+import static java.util.Arrays.asList;
+import java.util.List;
+
+import static actestDemo.domain.LightState.*;
 import static org.junit.Assert.*;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 import actestDemo.actest.LightStateEditor;
-import actestDemo.domain.LightState;
 
 /**
  * Created by sutaiyun on 2016/8/2.
  */
+@RunWith(Parameterized.class)
 public class LightStateEditorTest {
+
+    @Parameters
+    public static List<Object[]> data() {
+        return asList(new Object[][] {
+                { "RED",            RED },
+                { "RED, YELLOW",    RED_YELLOW }
+        });
+    }
+    /*
     private LightStateEditor editor = new LightStateEditor();
 
     @Test
     public void setEditor () {
         editor.setAsText("RED");
-        assertEquals(LightState.RED, editor.getValue());
+        assertEquals(RED, editor.getValue());
+    }
+    */
+
+    private LightStateEditor editor = new LightStateEditor();
+    private String stateName;
+    private LightState state;
+
+    public LightStateEditorTest(String stateName, LightState state) {
+        this.stateName = stateName;
+        this.state = state;
+    }
+
+    @Test
+    public void setAsText () {
+        editor.setAsText(stateName);                /* 模拟  网页wiki  设置 state 串 */
+        assertEquals(state, editor.getValue());     /* editor 是从wiki 网页上获取的string，然后转化成state对象 */
     }
 }
