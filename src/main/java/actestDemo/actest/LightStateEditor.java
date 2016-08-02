42749d193df416d700131209e3b6473c33c7a31b
sutaiyun <sutaiyun@longcheer.net>
2016-08-02 14:16:25 +0800

    增加  LightStateEditor 的业务代码，  使测试通过   mvn test  OK！！！！！！！

--- src/main/java/actestDemo/actest/LightStateEditor.java
+++ src/main/java/actestDemo/actest/LightStateEditor.java
@@ -8,8 +8,12 @@ import java.beans.PropertyEditorSupport;
  * Created by sutaiyun on 2016/8/2.
  */
 public class LightStateEditor extends PropertyEditorSupport {
-    public void setAsText(String state) {
-        setValue(LightState.RED);
+    public void setAsText(String stateName) {
+        if ("RED".equals(stateName)) {
+            setValue(LightState.RED);
+            return;
+        }
+        setValue(LightState.RED_YELLOW);
     }
 
     /* getValue() , return object
